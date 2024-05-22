0afe588f6f78b658b5824bd16c87a951142fb53b
masha <mariyapnmrv@gmail.com>
2024-05-22 16:16:07 -0500

    about sectoin 1.0

--- src/components/About.js
+++ src/components/About.js
@@ -1,6 +1,7 @@
 "use client";
 
 import Section from "./Section";
+import Image from "next/image";
 import { useEffect } from "react";
 
 export default function About() {
@@ -41,24 +42,119 @@ export default function About() {
   }, []);
 
   return (
-    <Section id="about" className="min-h-screen py-20">
-      <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-bold mb-8">About Me</h2>
-        <ul className="stack-cards js-stack-cards">
-          <li className="stack-cards__item js-stack-cards__item bg-white shadow-lg rounded-lg p-8 mb-4">
-            <h3 className="text-xl font-bold mb-2">Card 1</h3>
-            <p className="text-gray-700">Content for the first card...</p>
-          </li>
-          <li className="stack-cards__item js-stack-cards__item bg-white shadow-lg rounded-lg p-8 mb-4">
-            <h3 className="text-xl font-bold mb-2">Card 2</h3>
-            <p className="text-gray-700">Content for the second card...</p>
-          </li>
-          <li className="stack-cards__item js-stack-cards__item bg-white shadow-lg rounded-lg p-8 mb-4">
-            <h3 className="text-xl font-bold mb-2">Card 3</h3>
-            <p className="text-gray-700">Content for the third card...</p>
-          </li>
-        </ul>
-      </div>
+    <Section id="about" title="About">
+      <ul className="stack-cards js-stack-cards">
+        <li className="stack-cards__item js-stack-cards__item relative bg-[#C5F7FF] border-4 border-[#02A3DC] rounded-xl p-4 mb-6 mx-32 h-96 flex items-center justify-center overflow-hidden">
+          <Image
+            src="/blob1_1.svg"
+            alt="Blob 1"
+            width={200}
+            height={100}
+            className="absolute top-5 left-5 -translate-x-1/2 -translate-y-1/2"
+          />
+          <Image
+            src="/blob1_2.svg"
+            alt="Blob 2"
+            width={160}
+            height={80}
+            className="absolute bottom-5 right-10 translate-x-1/2 translate-y-1/2"
+          />
+          <div className="w-1/3 h-full  flex items-center justify-center">
+            <Image
+              src="/dev_avatar.svg"
+              alt="Developer Avatar"
+              width={200}
+              height={100}
+            />
+          </div>
+          <div className="w-2/3 h-full flex flex-col justify-center">
+            <h2 className="text-2xl font-bold mb-4 text-center">
+              From Figma to Full Stack
+            </h2>
+            <p className="font-normal leading-7 px-8">
+              Blending design with development, my approach to coding is deeply
+              rooted in a technical and aesthetic foundation. Proficient in the
+              MERN stack, I specialize in translating Figma designs into
+              functional, user-centric applications. My work reflects a seamless
+              fusion of form and function, where each line of code is both an
+              execution of strategy and a brushstroke of creativity.
+            </p>
+          </div>
+        </li>
+        <li className="stack-cards__item js-stack-cards__item relative bg-[#D5E2FF] border-4 border-[#7FA6FF] rounded-xl p-4 mb-4 mx-32 h-96 flex items-center justify-center overflow-hidden">
+          <Image
+            src="/blob2_1.svg"
+            alt="Blob 3"
+            width={200}
+            height={100}
+            className="absolute top-5 left-5 -translate-x-1/2 -translate-y-1/2"
+          />
+          <Image
+            src="/blob2_2.svg"
+            alt="Blob 4"
+            width={160}
+            height={80}
+            className="absolute bottom-5 right-10 translate-x-1/2 translate-y-1/2"
+          />
+          <div className="w-1/3 ml-8 h-full flex items-center justify-center">
+            <Image
+              src="/chem_avatar.svg"
+              alt="Chemistry Avatar"
+              width={210}
+              height={100}
+            />
+          </div>
+          <div className="w-2/3 h-full flex flex-col justify-center">
+            <h2 className="text-2xl font-bold mb-4 text-center">
+              Blending Chemistry with Code
+            </h2>
+            <p className="font-normal leading-7 px-8">
+              With a couple of chemistry degrees in my pocket and a knack for
+              material science, I spent some quality time in one of the top U.S.
+              labs. There, I played with complex compounds and dived deep into
+              data, all powered by a love for math. These skills turned out to
+              be just the right mix for software development, where I now blend
+              precision with creativity to cook up innovative solutions.
+            </p>
+          </div>
+        </li>
+        <li className="stack-cards__item js-stack-cards__item relative bg-[#FFE1F2] border-4 border-[#FF48B0] rounded-xl p-4 mb-4 mx-32 h-96 flex items-center justify-center overflow-hidden">
+          <Image
+            src="/blob3_1.svg"
+            alt="Blob 5"
+            width={200}
+            height={100}
+            className="absolute top-5 left-5 -translate-x-1/2 -translate-y-1/2"
+          />
+          <Image
+            src="/blob3_2.svg"
+            alt="Blob 6"
+            width={160}
+            height={80}
+            className="absolute bottom-5 right-10 translate-x-1/2 translate-y-1/2"
+          />
+          <div className="w-1/3 h-full ml-8 flex items-center justify-center">
+            <Image
+              src="/tennis_avatar.svg"
+              alt="Tennis Avatar"
+              width={190}
+              height={95}
+            />
+          </div>
+          <div className="w-2/3 h-full flex flex-col justify-center">
+            <h2 className="text-2xl font-bold mb-4 text-center">
+              Game, Set, Match
+            </h2>
+            <p className="font-normal leading-7 px-8">
+              Tennis was my first passion, taking me from the courts of Russia
+              to the competitive arenas of America. It taught me more than just
+              the game; it instilled discipline, adaptability, and the spirit of
+              teamwork. These lessons go beyond sports, influencing my approach
+              to every project and challenge in my professional life.
+            </p>
+          </div>
+        </li>
+      </ul>
     </Section>
   );
 }
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,8 +1,8 @@
 export default function Section({ id, title, children }) {
   return (
     <section id={id} className="min-h-screen">
-      <div className="container mx-auto px-4">
-        <h2 className="text-3xl font-semibold">{title}</h2>
+      <div className="container mx-auto">
+        <h2 className="text-3xl text-center mb-8 font-semibold">{title}</h2>
         {children}
       </div>
     </section>
