520d49ee77dd93e3d5ae0729dfaed80e50988316
masha <mariyapnmrv@gmail.com>
2024-05-27 12:53:20 -0500

    Moving eyes

---
+++ src/components/Eyes.js
@@ -0,0 +1,93 @@
+import { useEffect, useRef } from "react";
+import Image from "next/image";
+import styles from "./Eyes.module.css";
+
+const Eyes = () => {
+  const leftEyeRef = useRef(null);
+  const rightEyeRef = useRef(null);
+  const leftPupilRef = useRef(null);
+  const rightPupilRef = useRef(null);
+
+  useEffect(() => {
+    const handleMouseMove = (event) => {
+      const { clientX, clientY } = event;
+      movePupil(leftEyeRef.current, leftPupilRef.current, clientX, clientY);
+      movePupil(rightEyeRef.current, rightPupilRef.current, clientX, clientY);
+    };
+
+    window.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      window.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  const movePupil = (eye, pupil, mouseX, mouseY) => {
+    if (!eye || !pupil) return;
+
+    const eyeRect = eye.getBoundingClientRect();
+    const eyeCenterX = eyeRect.left + eyeRect.width / 2;
+    const eyeCenterY = eyeRect.top + eyeRect.height / 2;
+
+    const angle = Math.atan2(mouseY - eyeCenterY, mouseX - eyeCenterX);
+    const maxDistance = eyeRect.width / 4.5 - pupil.offsetWidth / 2;
+
+    const distanceX = mouseX - eyeCenterX;
+    const distanceY = mouseY - eyeCenterY;
+    const distance = Math.min(
+      maxDistance,
+      Math.sqrt(distanceX * distanceX + distanceY * distanceY)
+    );
+
+    const pupilX = eyeCenterX + distance * Math.cos(angle);
+    const pupilY = eyeCenterY + distance * Math.sin(angle);
+
+    pupil.style.left = `${pupilX - eyeRect.left - pupil.offsetWidth / 2}px`;
+    pupil.style.top = `${pupilY - eyeRect.top - pupil.offsetHeight / 2}px`;
+  };
+
+  return (
+    <div className={styles.eyes}>
+      <div className={styles.eye} ref={leftEyeRef}>
+        <Image
+          src="/left_eye.svg"
+          alt="Left Eye"
+          layout="fill"
+          objectFit="cover"
+        />
+        <div
+          className={`${styles.pupil} ${styles["left-pupil"]}`}
+          ref={leftPupilRef}
+        >
+          <Image
+            src="/pupil-left.svg"
+            alt="Left Pupil"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+      </div>
+      <div className={styles.eye} ref={rightEyeRef}>
+        <Image
+          src="/right_eye.svg"
+          alt="Right Eye"
+          layout="fill"
+          objectFit="cover"
+        />
+        <div
+          className={`${styles.pupil} ${styles["right-pupil"]}`}
+          ref={rightPupilRef}
+        >
+          <Image
+            src="/pupil-right.svg"
+            alt="Right Pupil"
+            layout="fill"
+            objectFit="contain"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Eyes;
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,17 +1,26 @@
+"use client";
+
 import Section from "./Section";
 import Image from "next/image";
+import Eyes from "./Eyes";
+import styles from "./Eyes.module.css";
 
 export default function Hero() {
   return (
     <Section id="hero">
-      <div className="text-center">
-        <Image
-          src="/avatar.png"
-          alt="Hero Image"
-          width={380}
-          height={190}
-          className="mx-auto mb-4"
-        />
+      <div className="text-center relative">
+        <div className="inline-block">
+          <Image
+            src="/avatar.svg"
+            alt="Hero Avatar"
+            width={380}
+            height={190}
+            className="mx-auto mb-4"
+          />
+          <div className={styles.eyes}>
+            <Eyes />
+          </div>
+        </div>
         <h1 className="text-3xl font-semibold pb-4">
           Hi, my name is Maria Key{" "}
         </h1>
