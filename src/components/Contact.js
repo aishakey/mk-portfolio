d7ea758fbd75353a1f84baf6ba71daf4eacfaccc
masha <mariyapnmrv@gmail.com>
2024-06-11 14:49:47 -0500

    Contact Form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,6 +1,7 @@
 "use client";
 
 import { useState } from "react";
+import Image from "next/image";
 
 export default function ContactForm() {
   const [name, setName] = useState("");
@@ -14,33 +15,53 @@ export default function ContactForm() {
   };
 
   return (
-    <div>
+    <div className="md:mt-10">
       <header
-        className="w-full h-40 bg-repeat-x bg-bottom"
+        className="w-full h-40 bg-repeat-x bg-bottom relative"
         style={{
           backgroundImage: "url('/wave.svg')",
         }}
       >
-        <h1 className="text-4xl font-bold text-center text-dark-blue">
-          Contact
-        </h1>
+        <div className="absolute bottom-0 left-1/2 transform -translate-x-1/2 mb-14 flex items-center">
+          <Image
+            src="/phone.svg"
+            alt="Phone"
+            width={28}
+            height={28}
+            className="mr-3 transform -scale-x-100"
+          />
+          <h1 className="md:text-2xl text-xl text-center font-semibold">
+            Let's get in touch
+          </h1>
+          <Image
+            src="/phone.svg"
+            alt="Phone"
+            width={28}
+            height={28}
+            className="ml-3"
+          />
+        </div>
       </header>
       <div className="relative flex flex-col justify-center items-center bg-[#C5F7FF]">
         <div
           id="form"
-          className="relative w-full max-w-[400px] md:max-w-[540px] p-4 bg-[#FFE1F2] rounded-xl mt-12"
+          className="relative w-full max-w-[400px] md:max-w-[450px] p-4 bg-[#FFE1F2] rounded-xl mt-12"
         >
-          <form id="waterform" onSubmit={handleSubmit} className="relative">
+          <form
+            id="waterform"
+            onSubmit={handleSubmit}
+            className="relative flex flex-col justify-center items-center"
+          >
             <div
               className={`formgroup ${
                 focusedField === "name" ? "formgroup-active" : ""
-              }`}
+              } flex flex-col items-center w-full`}
             >
               <label
                 htmlFor="name"
-                className="text-main-pink text-base block mt-3"
+                className="text-main-pink text-base block mt-3 w-7/12 -ml-4 md:-ml-16 text-left"
               >
-                Your name*
+                Your name
               </label>
               <input
                 type="text"
@@ -50,20 +71,20 @@ export default function ContactForm() {
                 onChange={(e) => setName(e.target.value)}
                 onFocus={() => setFocusedField("name")}
                 onBlur={() => setFocusedField(null)}
-                className="w-7/12 p-1 mt-1 rounded-2xl border-none outline-none bg-white text-main-pink text-sm"
+                className="w-7/12 md:w-8/12 p-2 mt-1 rounded-2xl border-none outline-none bg-white text-dark-blue font-normal text-sm"
                 required
               />
             </div>
             <div
               className={`formgroup mt-4 ${
                 focusedField === "email" ? "formgroup-active" : ""
-              }`}
+              } flex flex-col items-center w-full`}
             >
               <label
                 htmlFor="email"
-                className="text-main-pink text-base block mt-3"
+                className="text-main-pink text-base block mt-3 w-7/12 -ml-4 md:-ml-16 text-left"
               >
-                Your e-mail*
+                Your e-mail
               </label>
               <input
                 type="email"
@@ -73,18 +94,18 @@ export default function ContactForm() {
                 onChange={(e) => setEmail(e.target.value)}
                 onFocus={() => setFocusedField("email")}
                 onBlur={() => setFocusedField(null)}
-                className="w-7/12 p-1 mt-1 rounded-2xl border-none outline-none bg-white text-main-pink text-sm"
+                className="w-7/12 md:w-8/12 p-2 mt-1 rounded-2xl border-none outline-none bg-white text-dark-blue font-normal text-sm"
                 required
               />
             </div>
             <div
               className={`formgroup mt-4 ${
                 focusedField === "message" ? "formgroup-active" : ""
-              }`}
+              } flex flex-col items-center w-full`}
             >
               <label
                 htmlFor="message"
-                className="text-main-pink text-base block mt-3"
+                className="text-main-pink text-base block mt-3 -ml-4 md:-ml-16 w-7/12 text-left"
               >
                 Your message
               </label>
@@ -95,21 +116,19 @@ export default function ContactForm() {
                 onChange={(e) => setMessage(e.target.value)}
                 onFocus={() => setFocusedField("message")}
                 onBlur={() => setFocusedField(null)}
-                className="w-7/12 p-1 mt-1 rounded-2xl border-none outline-none bg-white text-main-pink text-base h-20 resize-none"
+                className="w-7/12 md:w-8/12 px-2 mt-1 rounded-2xl border-none outline-none bg-white text-dark-blue font-normal text-sm h-20 resize-none"
                 required
               ></textarea>
             </div>
-            <button
-              className="w-full p-2
-              mt-4 bg-pink-600 text-white text-sm rounded-2xl cursor-pointer
-              hover:bg-pink-700 transition-all"
-            >
-              Send
-            </button>
+            <div className="flex justify-center w-full mt-8 mb-3">
+              <button className="w-11/12 md:w-11/12 p-2 bg-main-pink text-white text-sm rounded-2xl cursor-pointer hover:bg-[#ed4baa] transition-all">
+                Send
+              </button>
+            </div>
           </form>
         </div>
-        <p className="text-[#8f8f8f] font-light text-sm text-center mt-4">
-          &copy; 2024. Designed and developed by Maria Key{" "}
+        <p className="text-[#a4a4a4] font-light text-sm text-center pt-8 pb-3">
+          &copy; 2024. Designed and developed by Maria Key
         </p>
       </div>
     </div>
